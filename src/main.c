c00c161ab21be7514342ce9c48ef54717557f924
OlegSadJktu <plyuha2004@mail.ru>
2022-09-12 00:13:53 +0600

    Add set data.

--- src/main.c
+++ src/main.c
@@ -16,28 +16,9 @@ int main(int argc, char **argv) {
     struct Vector *vec = create_vector(sizeof(SimpleData));
 
     const int size = 10;
-    SimpleData *sd;
-    for (int i = 0; i < size; i++) {
-        sd = malloc(sizeof(sd));// {i, i+1, i+2, i+3};
-        sd->first = i;
-        sd->second = i+1;
-        sd->third = i+2;
-        sd->p = &(sd->first);
-        print_data(sd);
+    SimpleData sd = {1,2,3, (int*) 4};
 
-        add_data(vec, sd);
-    }
-    for (int i = 0; i < size; i++) {
-        print_data(get_data(vec, i));
-    }
-    return 0;
+    add_data(vec, &sd);
 
-    /* printf("%i\n", vec->dataSize); */
-    /* printf("size array => %i  busy array => %i\n", vec->size, vec->busy); */
-    for (int i = 0; i < 100; i++) {
-        SimpleData *sd = get_data(vec, i);
-        printf("%i\n", sd->first);
-        /* printf("data => %i %i", sd->first, sd->second); */
-    }
     return 0;
 }
--- src/vector.c
+++ src/vector.c
@@ -3,14 +3,20 @@
 
 static const int EXPAND = 2;
 
-typedef struct Vector {
+
+
+static struct Vector {
     int size;
     int busy;
     int dataSize;
     void **data;
 } Vector;
 
-static void realocate_data(Vector *vec) {
+struct VectorWrapper {
+    struct Vector *vector;
+};
+
+static void realocate_data(struct Vector *vec) {
     if (vec->busy != vec->size) {
         exit(1);
     }
@@ -19,14 +25,14 @@ static void realocate_data(Vector *vec) {
     vec->size *= EXPAND;
 }
 
-void *get_data(Vector *vec, int pos) {
+void *get_data(struct Vector *vec, int pos) {
     if (pos < vec->busy) {
         return vec->data[pos];
     }
     exit(1);
 }
 
-void add_data(Vector *vec, void *data) {
+void add_data(struct Vector *vec, void *data) {
     if (vec->size == vec->busy) {
         realocate_data(vec);
     }
@@ -34,8 +40,23 @@ void add_data(Vector *vec, void *data) {
     vec->busy += 1;
 }
 
-Vector *create_vector(const int dataSize) {
-    Vector *vec = malloc(sizeof(Vector));
+void set_value(struct Vector *vec, void *data, int pos) {
+    void *d = vec->data[pos];
+    if (d == NULL && data == NULL) {
+        return;
+    }
+    if (d != NULL) {
+        vec->busy++;
+    }
+    if (data == NULL) {
+        vec->busy--;
+    }
+    vec->data[pos] = data;
+
+}
+
+struct Vector *create_vector(const int dataSize) {
+    struct Vector *vec = malloc(sizeof(struct Vector));
     int size = 8;
     vec->size = size;
     vec->busy = 0;
